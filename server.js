cbbb4e65e6c3eef33d4f0d1d1e4b463b2cadf5f4
Alan Kuzmanovic <alankuzmanovic@gmail.com>
2019-11-09 16:45:50 -0700

    heroku round 5

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT;
 
 // Initialize Express
 var app = express();
@@ -35,6 +35,9 @@ mongoose.connect(MONGODB_URI);
 
 // Routes
 
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
